74fead76fd054cf453c073247c25c0995ce6a882
Jerry Chen <jerry@localhost.localdomain>
2016-02-08 03:13:23 -0800

    Finished game_solver

--- game_solver.py
+++ game_solver.py
@@ -25,7 +25,7 @@ class Game_State:
     def __init__(self, state): 
         self.state = state
         self.next_states = []
-        self.curr_turn = "P1"
+        self.curr_turn = 1
     
     def genMoves(self):
         return gen_moves(self.state)
@@ -34,14 +34,14 @@ class Game_State:
         pos_moves = genMoves()
         states = do_moves(pos_moves)
         self.next_states = [Game_State(state) for state in states]
-        if self.curr_turn == "P1":
-            self.currturn = "P2"
+        if self.curr_turn == 1:
+            self.curr_turn = 2
         else:
-            self.curr_turn = "P1"
+            self.curr_turn = 1
 
 def generate_game_tree(game_pos):
     """Creates a tree from a game state"""
-    if primitive(game_pos):
+    if primitive(game_pos.state):
         return
     else:
         game_pos.doMoves()
@@ -50,5 +50,20 @@ def generate_game_tree(game_pos):
 
 
 
-def solve():
-    game_tree = generate_game_tree(initial_position)
+def solve(game_state):
+    if primitive(game_state.state):
+        return game_state.curr_turn
+    possibilites = [solve(next_turn) for next_turn in game_state.next_states]
+    if game_state.curr_turn in possibilites:
+        return game_state.curr_turn
+    if game_state.curr_turn == 1:
+        return 2
+    return 1
+
+def main():
+    game_tree = Game_State(initial_position)
+    generate_game_tree(game_tree)
+    solve(game_tree)
+
+main()
+
