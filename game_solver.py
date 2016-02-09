99e3216d52caa204517d7ea553c1368397e7dcd8
Jerry Chen <jerry@localhost.localdomain>
2016-02-08 17:35:39 -0800

    Added 4-1 as demo game

--- game_solver.py
+++ game_solver.py
@@ -7,16 +7,24 @@ do_moves(pos) = Run all the moves. Should return a list of next set of states
 Fill in functions and initial_position to use
 """
 
-initial_position = 0
+initial_position = 4
 
 def primitive(pos):
-    return
+    return pos == 0
 
 def gen_moves(pos):
-    return
+    def sub1(x):
+        return x-1
+    def sub2(x):
+        return x-2
+    if pos >= 2:
+        return [sub1, sub2]
+    else:
+        return [sub1];
 
-def do_moves(pos, move):
-    return
+def do_moves(pos, moves):
+    return [move(pos) for move in moves]
+                
 
 class Game_State:
     """Each state has its current state and list of next possible states
@@ -31,8 +39,8 @@ class Game_State:
         return gen_moves(self.state)
     
     def doMoves(self):
-        pos_moves = genMoves()
-        states = do_moves(pos_moves)
+        pos_moves = self.genMoves()
+        states = do_moves(self.state, pos_moves)
         self.next_states = [Game_State(state) for state in states]
         if self.curr_turn == 1:
             self.curr_turn = 2
@@ -46,7 +54,7 @@ def generate_game_tree(game_pos):
     else:
         game_pos.doMoves()
         for states in game_pos.next_states:
-            generate_tree(states)
+            generate_game_tree(states)
 
 
 
@@ -58,12 +66,14 @@ def solve(game_state):
         return game_state.curr_turn
     if game_state.curr_turn == 1:
         return 2
-    return 1
+    else:
+        return 1
 
 def main():
     game_tree = Game_State(initial_position)
     generate_game_tree(game_tree)
-    solve(game_tree)
+    return solve(game_tree)
 
-main()
+print(main())
+
 
